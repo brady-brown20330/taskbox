70856e79f9072f395df126a21d8b5295a04ff525
Brady <brown@Bradys-MacBook-Pro.local>
2021-10-14 09:05:14 -0400

    added button component

---
+++ src/components/Button.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import Button from './Button'
+
+export default {
+  title: 'Button',
+  component: Button
+};
+
+export const Text = () => <Button onClick={action('clicked')}>Hello Buton</Button>
+
+export const Emoji = () => (
+  <Button onClick={action('clicked')}>
+    <span role='img' aria-label='wow so cool'>
+      🤌 😎 🤌  
+    </span>
+  </Button>
+)
